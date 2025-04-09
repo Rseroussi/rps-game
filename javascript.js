6fc1bb5934293441350cbba6392ed8e55007dade
Rafael Seroussi <raf@rafaels-air.lan>
2025-04-08 20:00:13 -0500

    works once to play the game

--- javascript.js
+++ javascript.js
@@ -11,75 +11,90 @@ function getComputersChoice(){
 function getUsersChoice(){
     return prompt("choose rock, paper, or scissors:");
 }
+let human_score = 0;
+let computer_score = 0;
 
-
-function playRound(humanChoice, ComputerChoice){
+function playRound(humanChoice){
+    let outDiv = document.querySelector("#results");
+    let ComputerChoice = getComputersChoice();
     switch(humanChoice.toLowerCase()){
         case "rock":
             switch(ComputerChoice.toLowerCase()){
                 case "scissors":
                     
-                    console.log(`you win! ${humanChoice} beats ${ComputerChoice}`);
-                    return 1;
+                    outDiv.textContent = `you win! ${humanChoice} beats ${ComputerChoice}`;
+                    human_score++;
+                    break;
                 case "paper":
                     
-                    console.log(`you lose! ${humanChoice} loses to ${ComputerChoice}`);
-                    return 2;
+                    outDiv.textContent = `you lose! ${humanChoice} loses to ${ComputerChoice}`;
+                    computer_score++;
+                    break;
                 default:
-                    console.log('tie no one won!');
-                    return 0;
+                    outDiv.textContent = 'tie no one won!';
+                    
             }
             break;
         case "paper":
             switch(ComputerChoice.toLowerCase()){
                 case "rock":
                     
-                    console.log(`you win! ${humanChoice} beats ${ComputerChoice}`);
-                    return 1;
+                    outDiv.textContent =`you win! ${humanChoice} beats ${ComputerChoice}`;
+                    human_score++;
+                    break;
                 case "scissors":
                     
-                    console.log(`you lose! ${humanChoice} loses to ${ComputerChoice}`);
-                    return 2;
+                    outDiv.textContent =`you lose! ${humanChoice} loses to ${ComputerChoice}`;
+                    computer_score++;
+                    break;
                 default:
-                    console.log('tie no one won!');
-                    return 0;
+                    outDiv.textContent ='tie no one won!';
             }
             break;
             case "scissors":
                 switch(ComputerChoice.toLowerCase()){
                     case "paper":
                         
-                        console.log(`you win! ${humanChoice} beats ${ComputerChoice}`);
-                        return 1;
+                        outDiv.textContent =`you win! ${humanChoice} beats ${ComputerChoice}`;
+                        human_score++;
+                        break;
                     case "rock":
                         
-                        console.log(`you lose! ${humanChoice} loses to ${ComputerChoice}`);
-                        return 2;
+                        outDiv.textContent =`you lose! ${humanChoice} loses to ${ComputerChoice}`;
+                        computer_score++;
+                        break;
                     default:
-                        console.log('tie no one won!');
-                        return 0;
+                        outDiv.textContent ='tie no one won!';
+                        
                 }
                 break;
     }
 }
+let outDiv = document.querySelector("#results");
+let rockBtn = document.querySelector("#rock");
+let paperBtn = document.querySelector("#paper");
+let scissorsBtn = document.querySelector("#scissors");
 
+rockBtn.addEventListener("click", (event) => playRound("rock"));
+paperBtn.addEventListener("click", (event) => playRound("paper"));
+scissorsBtn.addEventListener("click", (event) => playRound("scissors"));
 function playGame(){
-    let human_score = 0;
-    let computer_score = 0;
-    for(let i = 0; i < 5; i++){
-        const humanSelection= getUsersChoice();
-        const computerSelection= getComputersChoice();
-        switch(playRound(humanSelection,computerSelection)){
-            case 1:
-                human_score++;
-                break;
-            case 2:
-                computer_score++;
-                break;
+    let outDiv = document.querySelector("#results");
+    if(human_score > 5 || computer_score > 5){
+        if(human_score > computer_score){
+            outDiv.textContent = "You won!";
+        } else {
+            outDiv.textContent = "Sorry the Computer won...";
         }
+        human_score = 0;
+        computer_score = 0;
+        return;
     }
+
+    setTimeout(playGame, 100);
+
     // console.log("final score: " + human_score + ", computer score:" + computer_score);
 }
 
-
+playGame();
 
